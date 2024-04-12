7c8d64ad0b1d2438872af8ad94e79cdebcc94c8a
Finn <finnprice03@gmail.com>
2024-04-11 22:02:25 -0700

    multiplayer with web sockets

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,91 @@
 const socket = io('ws://localhost:3000');
 
+const gridImage = new Image();
+gridImage.src = '/style/images/gridImage.png'
+
+const boat = new Image();
+boat.src = '/style/images/boat.png'
+
+const canvasEl = document.getElementById("canvas");
+canvasEl.width = window.innerWidth;
+canvasEl.height = window.innerHeight;
+const canvas = canvasEl.getContext("2d");
+
+let players = [];
+let leaderboard = [];
+
+const TILE_SIZE = 32;
+const MAP_SIZE = 100;
+const PLAYER_SIZE = TILE_SIZE * 3;
+
 socket.on('connect', () => {
     console.log('connected')
 })
+
+socket.on('players', (serverPlayers) => {
+    players = serverPlayers;
+})
+
+const inputs = {
+    left: false,
+    right: false
+}
+
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'a') {
+        inputs.left = true;
+    }
+    if(e.key === 'd') {
+        inputs.right = true;
+    }
+    socket.emit('inputs', inputs);
+})
+
+window.addEventListener('keyup', (e) => {
+    if(e.key === 'a') {
+        inputs.left = false;
+    }
+    if(e.key === 'd') {
+        inputs.right = false;
+    }
+    socket.emit('inputs', inputs);
+})
+
+
+function loop() {
+    canvas.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    
+    canvas.fillStyle = '#adb4db';
+    canvas.fillRect(0, 0, canvasEl.width, canvasEl.height);
+    const myPlayer = players.find((player) => player.id === socket.id);
+    let camX = 0;
+    let camY = 0;
+    if(myPlayer) {
+        camX = myPlayer.x - canvasEl.width / 2;
+        camY = myPlayer.y - canvasEl.height / 2;
+    }
+    console.log(camX, camY)
+    for (let row = 0; row < MAP_SIZE; row++) {
+        for (let col = 0; col < MAP_SIZE; col++) {
+            canvas.drawImage(gridImage,
+                col * TILE_SIZE - camX,
+                row * TILE_SIZE - camY,
+                TILE_SIZE,
+                TILE_SIZE);
+        }
+    }
+    
+    for(const player of players) {
+        console.log(player.rotation);
+        canvas.save();
+        canvas.translate(player.x - camX, player.y - camY);
+        canvas.rotate((player.rotation + 90) * Math.PI / 180);
+        canvas.translate(-PLAYER_SIZE / 2, -PLAYER_SIZE / 2);
+        canvas.drawImage(boat, 0, 0, PLAYER_SIZE, PLAYER_SIZE);
+        canvas.restore();        
+    }
+
+    window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
--- server.js
+++ server.js
@@ -7,10 +7,77 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer);
 
-io.on('connect', (socket) => {
-  console.log("user connected", socket.id);
-})
+const TICK_RATE = 90;
+
+let players = [];
+let inputsMap = {};
+
+const moveSpeed = 100;
+
+const baseRotSpeed = 90;
+
+let lastTickTime = Date.now();
+
+function tick() {
+  const now = Date.now();
+  const deltaTime = (now - lastTickTime) / 1000;
+  lastTickTime = now;
+
+  for (const player of players) {
+    const inputs = inputsMap[player.id];
+
+    if (inputs.right) {
+      player.rotation += baseRotSpeed * deltaTime;
+      player.rotation %= 360;
+    }
+    if (inputs.left) {
+      player.rotation -= baseRotSpeed * deltaTime;
+      if (player.rotation < 0) {
+        player.rotation += 360;
+      }
+    }
+
+    const angleInRadians = player.rotation * Math.PI / 180;
+    const dx = Math.cos(angleInRadians) * moveSpeed * deltaTime;
+    const dy = Math.sin(angleInRadians) * moveSpeed * deltaTime;
+
+    player.x += dx;
+    player.y += dy;
+  }
+
+  io.emit('players', players);
+}
+
+async function main() {
+  io.on('connect', (socket) => {
+    console.log("user connected", socket.id);
+
+    inputsMap[socket.id] = {
+      right: false,
+      left: false
+    }
+
+    players.push({
+      id: socket.id,
+      x: 0,
+      y: 0,
+      rotation: 0
+    });
+  
+    socket.on('inputs', (inputs) => {
+      inputsMap[socket.id] = inputs;
+    })
+
+    socket.on('disconnect', () => {
+      players = players.filter((player) => player.id !== socket.id)
+    })
+  })
+
+  setInterval(tick, 1000 / TICK_RATE);
+}
 
 app.use(express.static("public"));
 
 httpServer.listen(3000);
+
+main();
